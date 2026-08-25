926f9250db7788020eb7cc00044e75eda0eb2916
“jiayun” <jiayun_work@163.com>
2026-08-26 00:34:57 +0800

    fix: NitrogenBalance 读从未赋值的 liveN2.out，进出口氮对不上

--- internal/flame/result.go
+++ internal/flame/result.go
@@ -40,16 +40,8 @@ func (r *Result) EnthalpyResidual() float64 {
 }
 
 func (r *Result) NitrogenBalance() (inlet, outlet float64) {
-	inlet = r.Reactants.Get("N2")
-	outlet = liveN2.out
-	_ = r.Products
-	return inlet, outlet
+	return r.Reactants.Get("N2"), r.Products.Get("N2")
 }
-
-type n2Slot struct{ out float64 }
-
-var liveN2 n2Slot
-
 
 func (r *Result) DissociationUsed() bool {
 	return r.Config.Dissociation
